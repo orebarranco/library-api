e3b5deb06a9f6cfc38ba51a4c6aa0c749bb3f61a
Oreste Barranco Hernández <orebarranco90@gmail.com>
2026-05-30 00:01:16 -0400

    Add books() relationship to Author and Category models

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -9,6 +9,7 @@ use Database\Factories\AuthorFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @property string $id
@@ -44,5 +45,11 @@ final class Author extends Model
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
         ];
+    }
+
+    /** @return HasMany<Book, $this> */
+    public function books(): HasMany
+    {
+        return $this->hasMany(Book::class);
     }
 }
