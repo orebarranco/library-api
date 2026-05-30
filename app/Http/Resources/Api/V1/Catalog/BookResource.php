191de1f2e9e924890891e940e2d27cac2b05f6f3
Oreste Barranco Hernández <orebarranco90@gmail.com>
2026-05-30 00:14:09 -0400

    Add BookResource JSON:API format

---
+++ app/Http/Resources/Api/V1/Catalog/BookResource.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources\Api\V1\Catalog;
+
+use App\Models\Book;
+use Illuminate\Http\Resources\JsonApi\JsonApiResource;
+
+/**
+ * @property-read Book $resource
+ */
+final class BookResource extends JsonApiResource
+{
+    /**
+     * @var list<string>
+     */
+    public array $attributes = [
+        'title',
+        'isbn',
+        'publication_year',
+        'book_value',
+        'available_copies',
+    ];
+
+    /**
+     * @var list<string>
+     */
+    public array $relationships = [
+        'author',
+        'category',
+    ];
+}
